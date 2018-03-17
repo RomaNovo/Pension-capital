83509bfc3c4bb81471f4f995841c9a55e78633d5
RomaNovo <roma_1985_roma@mail.ru>
2018-03-17 22:54:32 +0200

    header mading

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,18 @@
+var iconElement = document.querySelectorAll('.brg-icon__elem');
+var icon = document.querySelector('.brg-icon');
+var menuBlock = document.querySelector('.burger-menu__list');
 
+icon.addEventListener('click', function() {
+	
+	iconElement.forEach(function(v) {
+		v.classList.toggle('brg-icon__elem_open');
+ 	});
+
+	if(iconElement[0].classList.contains('brg-icon__elem_open')) {
+	 	menuBlock.style.left = '20px';
+	}  else {
+		menuBlock.style.left = '-999px';
+	}
+
+})
+ 
--- ending/js/main.js
+++ ending/js/main.js
@@ -1,2 +1,18 @@
+var iconElement = document.querySelectorAll('.brg-icon__elem');
+var icon = document.querySelector('.brg-icon');
+var menuBlock = document.querySelector('.burger-menu__list');
 
-var xxx1 = document.getElementsByClassName('.btn1');
+icon.addEventListener('click', function() {
+	
+	iconElement.forEach(function(v) {
+		v.classList.toggle('brg-icon__elem_open');
+ 	});
+
+	if(iconElement[0].classList.contains('brg-icon__elem_open')) {
+	 	menuBlock.style.left = '20px';
+	}  else {
+		menuBlock.style.left = '-999px';
+	}
+
+})
+ 
